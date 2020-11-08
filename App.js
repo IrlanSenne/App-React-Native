032d6e3b9b5e3cf38103d46dbdb2acfd6614ae2c
Moreira <im.senne2017@gmail.com>
2020-11-08 18:04:48 +0000

    updates

--- App.js
+++ App.js
@@ -1,92 +1,99 @@
 import React, { useState }  from 'react';
-import { View,
-        Switch,
-        StyleSheet,
-        ImageBackground,
-        ActivityIndicator,
-        StatusBar,
-        Button  ,
-        SafeAreaView
-     } from 'react-native';
-import Lp from './listaPlana';
-import CxTx from './CaixaDeTexto';
-import Toque from './toque';
-import Modal from './Modal';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const imgBg1 = './assets/background.jpg'
-
- export default function App1(){    
-  
-     const [ligado,setLigado] = useState(true)
-     const toggleLigado = ()=>setLigado(!ligado)
-     const [barra,setBarra] = useState('blue')
-
-     const mudarCorBarra= ()=>{
-      setBarra('red')
-     }
+import { View, StyleSheet, Text, Button} from 'react-native';
 
 
-     return(       
-       <View style={{flex: 1,backgroundColor:'#fff',padding:20, color:'#eee'}}>   
-       {/* <ScrollView style={{backgroundColor:'#4155af',padding:20}}>   */}
-      <Switch
-        trackColor={{false:'#430000',true:'#00de00'}}
-        thumbColor={ligado ? '#0f0' : '#f00'}
-        value={ligado}
-        onValueChange={toggleLigado}
-      />
-     {ligado ?
-       <ImageBackground source={require(imgBg1)} style={estilos.ImgFundo}>
-            <View style={estilos.cont2}>
-               <Lp/>
-            </View>
-         </ImageBackground>   : <SafeAreaView
-         barStyle='default'
-         backgroundColor='rgba(45,150,78,0.3)'
-         >
-         
-         <StatusBar
-             backgroundColor={barra}
-             barStyle='default'
-             hidden={false}
-             animated={true} 
-             networkActivityIndicatorVisible={true}
-             translucent={true}
-             showHideTransition='fade'
-         />
-        <CxTx/>
-        <ActivityIndicator size="large" color="#fe5de0" />
-        <Toque/>
-        <Modal/>
-        <Button
-          title='Mudar cor'
-          onPress={mudarCorBarra}
-        />
+const Pilha = createStackNavigator();
 
-        </SafeAreaView>
-      }
-      {/* </ScrollView> */}
+function Vinhos({navigation}){
+  return(
+    <View style={{flex:1,alignItems:'center', justifyContent:'center'}}>
+    <Text>Vinhos</Text>
+    <Button
+      title='Ver sobremesas'
+      onPress={()=>navigation.navigate('Sobremesa')}
+    />
       
-       </View>
+    </View>
+
+  )
+}
+
+function Sobremesa({navigation}){
+  return(
+    <View style={{flex:1,alignItems:'center', justifyContent:'center'}}>
+    <Text>Sobremesa</Text>
+    <Button
+      title='Ver lista de vinhos'
+      onPress={()=>navigation.navigate('Vinhos')}
+    />
+    </View>
+
+
+  )
+}
+
+ function App1(){    
+  
+     return(       
+    //    <View style={{flex: 1,backgroundColor:'#fff',padding:20, color:'#eee'}}>   
+    //    {/* <ScrollView style={{backgroundColor:'#4155af',padding:20}}>   */}
+    //   <Switch
+    //     trackColor={{false:'#430000',true:'#00de00'}}
+    //     thumbColor={ligado ? '#0f0' : '#f00'}
+    //     value={ligado}
+    //     onValueChange={toggleLigado}
+    //   />
+    //  {ligado ?
+    //    <ImageBackground source={require(imgBg1)} style={estilos.ImgFundo}>
+    //         <View style={estilos.cont2}>
+    //            <Lp/>
+    //         </View>
+    //      </ImageBackground>   : <SafeAreaView
+    //      barStyle='default'
+    //      backgroundColor='rgba(45,150,78,0.3)'
+    //      >
+         
+    //      <StatusBar
+    //          backgroundColor={barra}
+    //          barStyle='default'
+    //          hidden={false}
+    //          animated={true} 
+    //          networkActivityIndicatorVisible={true}
+    //          translucent={true}
+    //          showHideTransition='fade'
+    //      />
+    //     <CxTx/>
+    //     <ActivityIndicator size="large" color="#fe5de0" />
+    //     <Toque/>
+    //     <Modal/>
+    //     <Button
+    //       title='Mudar cor'
+    //       onPress={mudarCorBarra}
+    //     />
+
+    //     </SafeAreaView>
+    //   }
+    //   {/* </ScrollView> */}
+    <NavigationContainer>
+      <Pilha.Navigator>
+        <Pilha.Screen
+          name='Vinhos'
+          component={Vinhos}
+         
+        />
+          <Pilha.Screen
+          name='Sobremesa'
+          component={Sobremesa}
+          
+        />
+      </Pilha.Navigator>
+    </NavigationContainer>
+    
      )
    
  }
-
- const estilos = StyleSheet.create({
-   logo:{
-     width:'100%',
-    resizeMode:'cover',
-    marginTop:330
-   },
-   ImgFundo:{
-     flex:1,
-     resizeMode:'cover',
-     width:'100%',
-    
-   },
-   cont2:{
-     padding:10,
-     flex:1,
-     justifyContent:'center',
-   }
- }) 
+ export default App1
+  
